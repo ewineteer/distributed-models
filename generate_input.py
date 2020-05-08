03880a0275fde85c974be4829dbc90c90551f9eb
ewineteer <eric.wineteer@paradigmh2o.com>
2020-05-07 19:43:12 -0700

    updated input generator to function on linux

--- generate_input.py
+++ generate_input.py
@@ -4,13 +4,19 @@ import pandas as pd
 import os
 import tqdm
 
-df = pd.read_csv('C:/Users/Eric W/Downloads/InputData-Oakwood_Rustic_05-06-20.csv',header=None)
+os.chdir('/home')
+
+os.getcwd()
+
+df = pd.read_csv('/home/eric/projects/InputData-WaterSupply_05-07-20.csv',header=None)
 df = df[df.index != 1]
 df = df[df.index != 2]
 df = df.reset_index()
+df.loc[0,pd.isna(df.loc[0,:])] = -1
 reprows = df.iloc[0]
 reprows = reprows.astype('int')
 reprows = reprows.astype('str')
+
 
 
 def gen_inp(template,reprows,repvals,filename):
@@ -26,17 +32,19 @@ def gen_inp(template,reprows,repvals,filename):
 
 
 
-
-batchfile = open('C:/Users/Eric W/Downloads/modrun.bat','w+' ,encoding='utf_8')
+batchfile = open('/home/eric/projects/wsmodrun.bat','w+' ,encoding='utf_8')
 
 for i in tqdm.trange(1,len(df.index)):
-    template = open('C:/Users/Eric W/Downloads/Template.inp','r',encoding='utf_8')
+    template = open('/home/eric/projects/Template.inp','r',encoding='utf_8')
     repvals = df.loc[i,].astype(str)
-    filename = df.loc[i,5]
-    filepath = 'C:/Projects/CityofLA_Opti/Input/WQ/'
-    filename = filepath + filename
+    origname = df.loc[i,5]
+    filepath = '/home/eric/projects/Input/WS/'
+    batchpath = 'C:/Projects/Input/WS/'
+    filename = filepath + origname
+    batchname = batchpath + origname
     gen_inp(template=template,reprows=reprows,repvals=repvals,filename=filename)
-    batchfile.write("Sustain64.exe 2 " + filename +'\n')
+    batchfile.write("Sustain64.exe 2 " + batchname +'\n')
+
 
 batchfile.write("pause\n")
 batchfile.write("exit")
